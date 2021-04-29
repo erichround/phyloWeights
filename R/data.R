dc41431cf4e78f58df5f14e3d7a47c40de82e348
Erich Round <e.round@uq.edu.au>
2021-04-29 21:34:49 +1200

    Added and documented Yin 2020 data

--- R/data.R
+++ R/data.R
@@ -26,3 +26,26 @@
 #' }
 #' @source \url{https://glottolog.org/meta/downloads}
 "glottolog_geography_v4.3"
+
+
+#' Sonority sequencing violation data in 496 languages, 
+#' from Yin (2020)
+#'
+#' A dataframe containing language identifiers and data
+#' on the presence of SSP violations in word-initial onsets
+#' and word-final codas, as used in:
+#' 
+#' Yin, Ruihua. 2020. “Violations of the Sonority Sequencing 
+#' Principle: How, and How Often?” 2020 Conference of the 
+#' Australian Linguistic Society, Brisbane. 
+#' https://als.asn.au/Conference/Program
+#'
+#' @format A dataframe
+#' @source Yin (2020)
+#' \describe{
+#'   \item{name}{name the lect}
+#'   \item{glottocode}{}
+#'   \item{onset_violation}{presence (1) or absence (0) of word-initial SSP violation}
+#'   \item{coda_violation}{presence (1) or absence (0) of word-final SSP violation}
+#'   
+"yin_2020_data"
